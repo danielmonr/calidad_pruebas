f60195ac1618a554841f81394e24645ab99e569a
Monzalvo <danielmonr@gmail.com>
2017-01-13 09:46:25 -0600

    clase 2

--- clase2/first_unit.cc
+++ clase2/first_unit.cc
@@ -1,14 +1,37 @@
 #include "almacen.cc"
+#include "time.h"
 #include "gtest/gtest.h"
 
-class AlmacenF: public testing::Test{
+class TimeF : public testing::Test{
+    public:
+    virtual void SetUp(){
+        tiempo = time(NULL);
+    }
+    virtual void TearDown(){
+        EXPECT_TRUE(time(NULL) - tiempo <= 10) << "tardo mucho tiempo\n";
+    }
+    private:
+    time_t tiempo;
+};
+
+class AlmacenF: public TimeF{
 	public:
 		void SetUp(){
+            TimeF::SetUp();
 		}
 		void TearDown(){
+            TimeF::TearDown();
 		}
-		Almacen* a = new Almacen(10);
+        static void SetUpTestCase(){
+            a = new Almacen(10);
+        }
+        static void TearDownTestCase(){
+            delete a;
+        }
+		static Almacen* a;
 };
+
+Almacen* AlmacenF::a = 0;
 
 TEST_F(AlmacenF, constructor){
 	ASSERT_EQ(10, a->lista->capacity());
@@ -16,11 +39,11 @@ TEST_F(AlmacenF, constructor){
 
 TEST_F(AlmacenF, add){
 	a->add(3);
+    sleep(11);
 	ASSERT_EQ(1, a->lista->size());
 }
 
 TEST_F(AlmacenF, delet){
-    a->add(3);
 	a->delet(0);
 	ASSERT_EQ(0, a->lista->size());
 }
